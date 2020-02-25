09f5ebba7c27a5bcf61c03420483d329e6fe9c32
Ronin Protheroe <46990941+kiwiscripter@users.noreply.github.com>
2020-02-25 08:03:04 -0600

    Add files via upload

--- engine.js
+++ engine.js
@@ -1,32 +1,35 @@
-var c = document.getElementById("game");
-var ctx = c.getContext("2d");
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
 let posX = 10;
 let posY = 10;
 
-window.addEventListener('keydown', event => {
-    if (event.key === 'ArrowRight') {
-      posX = (posX + 1);
-    }
-    if (event.key === 'ArrowLeft') {
-        posX = (posX - 1);
-      }
-      if (event.key === 'ArrowUp') {
-        posY = (posY - 1);
-      }
-      if (event.key === 'ArrowDown') {
-          posY = (posY + 1);
-        }
-  })
+const keybinds = {
+    ArrowRight: () => posX++,
+    ArrowLeft: () => posX--,
+    ArrowUp: () => posY--,
+    ArrowDown: () => posY++,
+};
 
-function refresh(){
-    var container = document.getElementById("game");
-    container.innerHtml="";
+window.addEventListener('keydown', event => {
+    if (typeof keybinds[event.key] === 'function') keybinds[event.key]();
+});
+
+function refresh() {
     ctx.beginPath();
-ctx.lineWidth = "10";
-ctx.strokeStyle = "blue";
-ctx.rect(posX, posY, 5, 5);
-ctx.stroke();
+    ctx.lineWidth = '10';
+    ctx.strokeStyle = 'blue';
+    ctx.rect(posX, posY, 5, 5);
+    ctx.stroke();
 }
-function clearCanvas(){  c.width = c.width;}
-window.setInterval(clearCanvas, 1000);
-window.setInterval(refresh, 100);
+
+function clearCanvas() {
+    canvas.width = canvas.width;
+}
+
+function main() {
+    clearCanvas();
+    refresh();
+    requestAnimationFrame(main);
+}
+
+requestAnimationFrame(main);
